77e64ce7d1888e94f5e1d43d4ca213619bb3b1e5
colrick <62342916+colrick@users.noreply.github.com>
2020-06-05 11:32:55 +0200

    Add files via upload

---
+++ Mzamomtsha primary school website/homepageJS.js
@@ -0,0 +1,30 @@
+function homePage() {
+	
+	var welcomeText = "Welcome To The Mzamomtsha Primary School Website!"
+
+alert (welcomeText);
+}
+
+function newPicture() {
+	
+document.getElementById("image").src="Images/mzamomtsha2.jpg";
+	
+}
+
+function oldPicture() {
+	
+	document.getElementById("image").src="Images/Mzamomtsha (1).jpg";
+	
+}
+
+function newPicture1() {
+	
+document.getElementById("image1").src="Images/mzamomtsha3.jpg";
+	
+}
+
+function oldPicture1() {
+	
+	document.getElementById("image1").src="Images/Mzamomtsha (2).jpg";
+	
+}
